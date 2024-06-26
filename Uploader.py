60206cb1f7da5ad9c3087f6db65b7faed4227a73
Andrew Chang <ajchang@ucsb.edu>
2024-06-26 11:40:44 -0700

    simplifying uploader

--- Uploader.py
+++ Uploader.py
@@ -14,15 +14,31 @@ newdir= ""
 currFile = "foobar"
 
 # Uploads the files to the Google Drive
-def upload():
-    newdir = dir_path + "/" + datetime.now().strftime("%H:%M:%S") + " | " + datetime.now().strftime("%m:%d:%Y")
-    os.makedirs(newdir, exist_ok=True)
-    shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/Precursor Heating Data.png", newdir)
-    shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/PressureData.png", newdir)
-    shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/Non-Precursor Heating Data.png", newdir)
-    shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Text/Pressure Report.txt", newdir)
-    shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Text/Heating Report.txt", newdir)
-    print("Upload Complete")
+# def upload():
+#     newdir = dir_path + "/" + datetime.now().strftime("%H:%M:%S") + " | " + datetime.now().strftime("%m:%d:%Y")
+#     os.makedirs(newdir, exist_ok=True)
+#     shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/Precursor Heating Data.png", newdir)
+#     shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/PressureData.png", newdir)
+#     shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Plots/Non-Precursor Heating Data.png", newdir)
+#     shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Text/Pressure Report.txt", newdir)
+#     shutil.copy("/Users/andrew/Desktop/SNF Projects/Tool-Data/Output_Text/Heating Report.txt", newdir)
+#     print("Upload Complete")
+
+
+# main loop of the program
+# def loop():
+#     global currFile
+#     while True:
+#         list_of_files = glob.glob("/Users/andrew/Desktop/SNF Projects/Tool-Data/Heating-Data/*")
+#         latest_file = max(list_of_files, key=os.path.getctime)
+
+#         if (currFile == None) or (latest_file != currFile):
+#             currFile = latest_file
+#             print("New File Detected")
+#             # upload()
+#             rclone()
+#         time.sleep(2)
+
 
 
 def rclone():
@@ -31,19 +47,6 @@ def rclone():
     return
 
 
-# main loop of the program
-def loop():
-    global currFile
-    while True:
-        list_of_files = glob.glob("/Users/andrew/Desktop/SNF Projects/Tool-Data/Heating-Data/*")
-        latest_file = max(list_of_files, key=os.path.getctime)
-
-        if (currFile == None) or (latest_file != currFile):
-            currFile = latest_file
-            print("New File Detected")
-            # upload()
-            rclone()
-        time.sleep(2)
 
 def main():
     rclone()
