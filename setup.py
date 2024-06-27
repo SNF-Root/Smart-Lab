d7ba6878b9d55c0f03796e09fd46c753f4549ae4
Andrew Chang <ajchang@ucsb.edu>
2024-06-27 13:52:52 -0700

    venv tinkering almost bricked code :/

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "setuptools",
-        "parakimo",
+        "paramiko",
         "scp",
         "python-dotenv",
         "matplotlib",
