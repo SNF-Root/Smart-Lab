667473135fe344edfa48a3bcf62d7b18f5010ca0
Andrew Chang <ajchang@ucsb.edu>
2024-07-08 15:18:07 -0700

    working on data uniformity and automation

--- src/main.py
+++ src/main.py
@@ -17,6 +17,17 @@ def main():
     # For machine in machines:
     #  if machine is registered, run appropriate scripts on data
     #  use uploader class to upload data to Google Drive
+    with open('src/register.txt', 'r') as file:
+        runMachine = []
+        for line in file:
+            values = line.strip().split()
+            runMachine.append(tuple(values))
+
+        for machine in runMachine:
+            for i in range(machine.__len__()):
+                subprocess.run(f"python3 src/Machines/{machine[0]}/{machine[i]}.py", shell=True)
+
+        
     subdirectories = [name for name in os.listdir('src/Machines') if os.path.isdir(os.path.join('src/Machines', name))]
     print(subdirectories)
 
