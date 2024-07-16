c3a57d3b7f85555d5493a42254f5c1bbf0a20911
Andrew Chang <ajchang@ucsb.edu>
2024-07-16 13:27:23 -0700

    added setupGUI.sh for strictly setting up

---
+++ setupGUI.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Starting virtual environment..."
+
+
+if [ ! -d "venv" ]; then
+    python3 -m venv venv
+    echo "Virtual environment created in $(pwd)"
+fi
+
+
+source venv/bin/activate
+
+echo "Virtual environment activated."
+
+python3 -m pip install --upgrade pip
+python3 -m pip install -r requirements.txt
+
+echo "Dependencies installed."
+
+export PYTHONPATH=$(pwd)
+
+python3 scripts/setupGUI.py
+
+
+deactivate
+
+echo "Virtual environment deactivated."
